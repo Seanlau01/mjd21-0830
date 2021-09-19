f642e51911f1b0fb4eec6e41266523edb5e48dbe
Seanlau <lausean561@gmail.com>
2021-09-19 15:55:00 +0800

    完成轮播图

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function goPAGE() {
-    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-        window.location.href="index.html"
-
-    }
-    else {
-        window.location.href="https://seanlau01.github.io/taobao21-08-05/index.html"
-       
-    }
-}
-
+window.addEventListener('load',function(){
+    
+})
