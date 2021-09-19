9ed5d5a9c3ee3433d9885a7fc01641f692f33704
Seanlau <lausean561@gmail.com>
2021-09-19 21:52:46 +0800

    引入swiper插件，重构轮播图

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 window.addEventListener('load',function(){
-    
+    var swiper = new Swiper('.swiper-container', {
+        direction:'horizontal',
+          centeredSlides: true,
+          loop: true,
+        effect:'overflow',
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        autoplay: {
+            delay: 1000,
+            stopOnLastSlide: false,
+            disableOnInteraction: true,
+            },
+      });
 })
--- slide.js
+++ slide.js
@@ -1,42 +1,42 @@
-const CIR_LIS=document.querySelectorAll('.circles li')
-const SLI_BX=document.querySelector('.bx')
-const SLI_LIS=SLI_BX.querySelector('li')
-let index=0
-let timer=null
-let current=0
-listenCirClicked(CIR_LIS)
-autoplay()
+// const CIR_LIS=document.querySelectorAll('.circles li')
+// const SLI_BX=document.querySelector('.bx')
+// const SLI_LIS=SLI_BX.querySelector('li')
+// let index=0
+// let timer=null
+// let current=0
+// listenCirClicked(CIR_LIS)
+// autoplay()
 
 
-function rmActive(item){
-    for(let j=0;j<item.length;j++){
-        item[j].classList.remove('active')
-    }
-}
-function listenCirClicked(item){
-    for(let i=0;i<item.length;i++){
-        item[i].addEventListener('click',function(){
-            rmActive(item)
-            this.classList.add('active')
-            index=i
-            goto(SLI_BX,index)
-        })
-    }
-}
+// function rmActive(item){
+//     for(let j=0;j<item.length;j++){
+//         item[j].classList.remove('active')
+//     }
+// }
+// function listenCirClicked(item){
+//     for(let i=0;i<item.length;i++){
+//         item[i].addEventListener('click',function(){
+//             rmActive(item)
+//             this.classList.add('active')
+//             index=i
+//             goto(SLI_BX,index)
+//         })
+//     }
+// }
 
-function goto(item,index){
-    current= -12.5*index
-    item.style.transform="translateX("+ current +"%)"
-}
-function autoplay(){
-    timer=null
-    timer=setInterval(function(){
-    index++
-    if(index==8){
-        index=0
-    } 
-    rmActive(CIR_LIS)
-    CIR_LIS[index].classList.add('active')
-    goto(SLI_BX,index)
-   },1000)
-}
+// function goto(item,index){
+//     current= -12.5*index
+//     item.style.transform="translateX("+ current +"%)"
+// }
+// function autoplay(){
+//     timer=null
+//     timer=setInterval(function(){
+//     index++
+//     if(index==8){
+//         index=0
+//     } 
+//     rmActive(CIR_LIS)
+//     CIR_LIS[index].classList.add('active')
+//     goto(SLI_BX,index)
+//    },1000)
+// }
